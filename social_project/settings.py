9657ae0c62ef27ab02cdfc78d789d41735acbe4f
Guzman Ojero <guzman.ojero@gmail.com>
2024-08-31 22:19:17 -0300

    some changes that i dont remember

--- social_project/settings.py
+++ social_project/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static_url/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 

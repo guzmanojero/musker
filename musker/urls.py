239feb142f8788035c7f15dc7ef71fbd47debd31
Guzman Ojero <guzman.ojero@gmail.com>
2024-10-09 21:54:04 -0300

    fixed importing handler view & added go home in 404html

--- musker/urls.py
+++ musker/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.conf.urls import handler404
 
 from . import views
-from musker.views import handling_404
+
+# from musker.views import handling_404
 
 urlpatterns = [
     path("", views.home, name="home"),
